a6905700ac69cc6346901361a063475416155fb1
satvikDesktop <satviksingh2304@gmail.com>
2022-01-30 10:01:17 +0530

    project commit

---
+++ src/main/java/com/android/todo_list/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.android.todo_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.todo_list.R
+import com.android.todo_list.Todo
+import com.android.todo_list.TodoAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+}
---
+++ src/main/java/com/android/todo_list/Todo.kt
@@ -0,0 +1,7 @@
+package com.android.todo_list
+
+data class Todo (
+    val title: String,
+    var isChecked: Boolean =false
+)
+
